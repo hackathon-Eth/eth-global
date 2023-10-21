29a88b3630ed880afb20e0872848f3b54168c4e8
jainl28patel <jainilpatel2003@gmail.com>
2023-10-21 18:04:30 +0530

    add web3auth

--- frontend/src/components/web3auth.jsx
+++ frontend/src/components/web3auth.jsx
@@ -1,83 +1,83 @@
-// import React, { useEffect, useState } from "react";
-// import { Web3Auth } from "@web3auth/web3auth";
-// import { CHAIN_NAMESPACES } from "@web3auth/base";
+import React, { useEffect, useState } from "react";
+import { Web3Auth } from "@web3auth/web3auth";
+import { CHAIN_NAMESPACES } from "@web3auth/base";
 
-// const Web3AuthButton = () => {
-//     const [web3auth, setWeb3auth] = useState(null);
-//     const [provider, setProvider] = useState(null);
-//     const [address, setAddress] = useState("");
-//     const [balance, setBalance] = useState("");
-//     const [chainId, setChainId] = useState("");
-//     const [userData, setUserData] = useState({});
+const Web3AuthButton = () => {
+    const [web3auth, setWeb3auth] = useState(null);
+    const [provider, setProvider] = useState(null);
+    const [address, setAddress] = useState("");
+    const [balance, setBalance] = useState("");
+    const [chainId, setChainId] = useState("");
+    const [userData, setUserData] = useState({});
 
-//     const clientId = "BEMcxTB9tqdm7Mf6s2fkyQFMuFdojmyGBkpOhGr34mND0wTNvPSQ__1MwXrZH09qofJmqlq9sOTV4vGu3iBNhs0";
+    const clientId = "BEMcxTB9tqdm7Mf6s2fkyQFMuFdojmyGBkpOhGr34mND0wTNvPSQ__1MwXrZH09qofJmqlq9sOTV4vGu3iBNhs0";
 
-//     useEffect(() => {
-//         const init = async () => {
-//           try {
-//             const web3auth = new Web3Auth({
-//               clientId,
-//               chainConfig: {
-//                 chainNamespace: CHAIN_NAMESPACES.EIP155,
-//                 chainId: "0x13881",
-//                 rpcTarget: "https://rpc-mumbai.maticvigil.com/",
-//               },
-//             });
+    useEffect(() => {
+        const init = async () => {
+          try {
+            const web3auth = new Web3Auth({
+              clientId,
+              chainConfig: {
+                chainNamespace: CHAIN_NAMESPACES.EIP155,
+                chainId: "0x13881",
+                rpcTarget: "https://rpc-mumbai.maticvigil.com/",
+              },
+            });
     
-//             setWeb3auth(web3auth);
-//             await web3auth.initModal();
-//             setProvider(web3auth.provider);
-//           } catch (error) {
-//             // Handle the error
-//             console.log(error);
-//           }
-//         };
+            setWeb3auth(web3auth);
+            await web3auth.initModal();
+            setProvider(web3auth.provider);
+          } catch (error) {
+            // Handle the error
+            console.log(error);
+          }
+        };
     
-//         init();
-//     }, []);
+        init();
+    }, []);
 
 
-//       const login = async () => {
-//         if (!web3auth) {
-//           return;
-//         }
-//         const web3authProvider = await web3auth.connect();
-//         setProvider(web3authProvider);
-//       };
+      const login = async () => {
+        if (!web3auth) {
+          return;
+        }
+        const web3authProvider = await web3auth.connect();
+        setProvider(web3authProvider);
+      };
 
-//       const logout = async () => {
-//         if (!web3auth) {
-//           console.log("web3auth not initialized yet");
-//           return;
-//         }
-//         const web3authProvider = await web3auth.logout();
-//         setProvider(web3authProvider);
-//         setBalance("");
-//         setAddress("");
-//         setUserData({});
-//         setChainId("");
-//       };
+      const logout = async () => {
+        if (!web3auth) {
+          console.log("web3auth not initialized yet");
+          return;
+        }
+        const web3authProvider = await web3auth.logout();
+        setProvider(web3authProvider);
+        setBalance("");
+        setAddress("");
+        setUserData({});
+        setChainId("");
+      };
 
-//       const getUserInfo = async () => {
-//         if (!web3auth) {
-//           console.log("web3auth not initialized yet");
-//           return;
-//         }
-//         const user = await web3auth.getUserInfo();
-//         setUserData(user);
-//         console.log(user);
-//       };
+      const getUserInfo = async () => {
+        if (!web3auth) {
+          console.log("web3auth not initialized yet");
+          return;
+        }
+        const user = await web3auth.getUserInfo();
+        setUserData(user);
+        console.log(user);
+      };
 
-//     return (
-//         <div>
-//             <button onClick={login}>Login</button>
-//             <button onClick={logout}>Logout</button>
-//             <button onClick={getUserInfo}>Get User Info</button>
-//             <p>Address: {userData.address}</p>
-//             <p>Balance: {userData.balance}</p>
-//             <p>ChainId: {userData.chainId}</p>
-//         </div>
-//     );
-// }
+    return (
+        <div>
+            <button onClick={login}>Login</button>
+            <button onClick={logout}>Logout</button>
+            <button onClick={getUserInfo}>Get User Info</button>
+            <p>Address: {userData.address}</p>
+            <p>Balance: {userData.balance}</p>
+            <p>ChainId: {userData.chainId}</p>
+        </div>
+    );
+}
 
-// export default Web3AuthButton;
+export default Web3AuthButton;
