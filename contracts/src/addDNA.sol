437aff48cab97383d595c45c5ca734018f560a79
Ashutosh <cyberboy9971@gmail.com>
2023-10-22 17:16:24 +0530

    Minor corrections

--- contracts/src/addDNA.sol
+++ contracts/src/addDNA.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.8.19;
 
-import {UltraVerifier} from "userVerifier/plonk_vk.sol";
+import {UltraVerifier} from "./userVerifier/plonk_vk.sol";
 
 contract AddDNA {
     UltraVerifier verifier;
@@ -11,10 +11,10 @@ contract AddDNA {
     }
 
     function addDNA(
-        uint256[] memory _proof,
-        uint256[] memory _publicInputs,
+        bytes memory _proof,
+        bytes32[] memory _publicInputs,
         uint256[] memory _dna
-    ) public returns (bool) {
+    ) public view returns (bool) {
         require(_dna.length == 25, "DNA must be 25 characters");
         require(verifier.verify(_proof, _publicInputs),"Invalid proof and public inputs");
         
