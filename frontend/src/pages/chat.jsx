13f13a11bf009720a29b124cf7c22f690a79253a
jainl28patel <jainilpatel2003@gmail.com>
2023-10-22 18:59:39 +0530

    integrate chat

--- frontend/src/pages/chat.jsx
+++ frontend/src/pages/chat.jsx
@@ -1,49 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import './chat.css';
+import {connecttoXmtp, streamChat1, streamChat2} from '../components/helper.js'
+
+const ethers = require("ethers")
 
 const ChatApp = () => {
   const [activeChat, setActiveChat] = useState(null);
   const [message, setMessage] = useState('');
   const [chats, setChats] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [signer, setSigner] = useState(null);
+  const [xmtpInstance, setXmtpInstance] = useState(null);
+  const [destAddress, setDestAddress] = useState(null);
+  const [conversation, setConversation] = useState(null);
+  const [idx, setIdx] = useState(0);
 
   useEffect(() => {
     // Simulate loading the chats
-    setTimeout(() => {
-      setChats([
-        {
-          id: 1,
-          walletAddress: '0x1234',
-          messages: [
-            { text: 'Hi there!', isUser: false },
-            { text: 'Hello!', isUser: true },
-          ],
-        },
-        {
-          id: 2,
-          walletAddress: '0x2323214',
-          messages: [
-            { text: 'Hey!', isUser: false },
-          ],
-        },
-      ]);
+      const timeoutId = setTimeout(() => {
+      setChats(streamChat2);
+      console.log(streamChat2);
       setIsLoading(false);
     }, 2000);
   }, []);
 
-  const handleChatClick = (chatId) => {
+  // write a function connectWallet to connect to metamask wallet and store the wallet in state
+  const connectWallet = async () => {
+    if (window.ethereum === undefined) {
+      alert("Metamask not installed");
+      return;
+    }
+    try {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      setSigner(signer);
+    }
+    catch(err) {
+      console.log(err);
+      alert("Error connecting to metamask : ", err);
+    }
+  }
+
+  // write a function connectXmtp to connect to xmtp and store the xmtp instance in state
+  // const connectXmtp = async () => {
+  //   if(signer === null) {
+  //     alert("Please connect wallet first");
+  //     return;
+  //   }
+  //   try {
+  //     const xmtp = await Client.create(signer);
+  //     setXmtpInstance(xmtp);
+  //     const conv = await xmtp.conversations.newConversation(destAddress);
+  //     setConversation(conv);
+
+  //   } catch(err) {
+  //     alert("Error connecting to xmtp");
+  //   }
+  // };
+
+  const handleChatClick = async (chatId) => {
+    // setDestAddress('0x1234')
+      connectWallet();
+    // if(xmtpInstance === null) {
+    //   connecttoXmtp();
+    // }
     setActiveChat(chatId);
   };
 
-  const handleSendMessage = () => {
+  const handleSendMessage = async () => {
     if (message.trim() === '') return;
 
-    const updatedChats = chats.map((chat) => {
-      if (chat.id === activeChat) {
-        chat.messages.push({ text: message, isUser: true });
-      }
-      return chat;
-    });
+    let updatedChats = [...chats];
+    let newMsg = streamChat1[0].messages[idx];
+    setIdx(idx+1);
+    console.log(newMsg);
+    updatedChats[0].messages.push(newMsg);
 
     setChats(updatedChats);
     setMessage('');
