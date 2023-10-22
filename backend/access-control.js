9a31044c418fe3bdbe2a9e072cb970c23fa0741c
Tanmay Bajaj <tanmaybajaj567@gmail.com>
2023-10-22 16:44:02 +0530

    lit control condtion

--- backend/access-control.js
+++ backend/access-control.js
@@ -1,13 +1,16 @@
-const accessControlConditions = [{
-  contractAddress: "",
-  standardContractType: "",
-  chain: "ethereum",
-  method: "eth_getBalance",
-  parameters: [":userAddress", "latest"],
-  returnValueTest: {
-    comparator: "<=",
-    value: "1000000000000", // 0.000001 ETH
-  },
-}]
+import go from go.js
 
+var accessControlConditions = [
+  {
+    contractAddress: "",
+    standardContractType: "",
+    chain: "ethereum",
+    method: "go",
+    parameters: ["0x742d35Cc6634C0532925a3b844Bc454e4438f44e", ":userAddress"],
+    returnValueTest: {
+      comparator: "=",
+      value: "true",
+    },
+  },
+];
 export default accessControlConditions
